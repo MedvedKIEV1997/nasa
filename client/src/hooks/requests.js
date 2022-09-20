f258c3b4271eb9813c196344eb64a7ba4c209c5a
MedvedKIEV1997 <burdakigor@gmail.com>
2022-09-20 17:40:13 +0300

    docker implemented

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,44 +1,44 @@
-const API_URL = "http://localhost:6969/v1";
+const API_URL = 'v1';
 
 async function httpGetPlanets() {
-  const response = await fetch(`${API_URL}/planets`);
-  return await response.json();
+    const response = await fetch(`${API_URL}/planets`);
+    return await response.json();
 }
 
 async function httpGetLaunches() {
-  const response = await fetch(`${API_URL}/launches`);
-  const fetchedLaunches = await response.json();
-  return fetchedLaunches.sort((a, b) => {
-    return a.flightNumber - b.flightNumber;
-  });
+    const response = await fetch(`${API_URL}/launches`);
+    const fetchedLaunches = await response.json();
+    return fetchedLaunches.sort((a, b) => {
+        return a.flightNumber - b.flightNumber;
+    });
 }
 
 async function httpSubmitLaunch(launch) {
-  try {
-    return await fetch(`${API_URL}/launches`, {
-      method: "post",
-      body: JSON.stringify(launch),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (error) {
-    return {
-      ok: false,
-    };
-  }
+    try {
+        return await fetch(`${API_URL}/launches`, {
+            method: 'post',
+            body: JSON.stringify(launch),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    } catch (error) {
+        return {
+            ok: false
+        };
+    }
 }
 
 async function httpAbortLaunch(id) {
-  try {
-    return await fetch(`${API_URL}/launches/${id}`, {
-      method: "delete",
-    });
-  } catch (error) {
-    return {
-      ok: false,
-    };
-  }
+    try {
+        return await fetch(`${API_URL}/launches/${id}`, {
+            method: 'delete'
+        });
+    } catch (error) {
+        return {
+            ok: false
+        };
+    }
 }
 
 export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
